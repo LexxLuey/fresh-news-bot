2c1146e5a28df99f88140efd295f9e0637757f52
lexxluey <biggestluey@gmail.com>
2024-04-19 11:53:50 +0100

    deployment update

--- models/image_downloader.py
+++ models/image_downloader.py
@@ -1,27 +1,40 @@
+import logging
 import os
-
 import requests
 
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+logger = logging.getLogger(__name__)
 class ImageDownloader:
 
     @classmethod
     def download_news_picture(self, url, picture_name):
-        if not os.path.exists("output/pictures"):
-            os.makedirs("output/pictures")
+        logger.info("downloading image...")
+        try:            
+            if not os.path.exists("output/pictures"):
+                os.makedirs("output/pictures")
 
-        response = requests.get(url)
+            response = requests.get(url)
 
-        _, file_extension = os.path.splitext(url)
+            _, file_extension = os.path.splitext(url)
 
-        if file_extension.lower() not in [".jpg", ".jpeg", ".png"]:
-            file_extension = ".jpg"
+            if file_extension.lower() not in [".jpg", ".jpeg", ".png"]:
+                file_extension = ".jpg"
 
-        picture_filename = os.path.join(
-            "output/pictures", f"{picture_name}{file_extension}"
-        )
+            picture_filename = os.path.join(
+                "output/pictures", f"{picture_name}{file_extension}"
+            )
 
-        with open(picture_filename, "wb") as file:
-            file.write(response.content)
+            with open(picture_filename, "wb") as file:
+                file.write(response.content)
 
-        return picture_filename
+            logger.info("downloading complete...")
+            return picture_filename
+
+        except Exception:
+            logger.exception("Could not download image...")
+
