e917a7df1ea60e1e912fdfb414d06a7585d5dba6
lexxluey <biggestluey@gmail.com>
2024-04-19 11:55:38 +0100

    deployment update

--- models/newscraper.py
+++ models/newscraper.py
@@ -28,7 +28,7 @@ logger = logging.getLogger(__name__)
 
 class NewsSiteScraper:
 
-    browser = Selenium(auto_close=False)
+    browser = Selenium()
     url = "https://www.latimes.com/"
     excel = ExcelWriter
 
